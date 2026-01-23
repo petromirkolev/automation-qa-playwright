18a03178b0f31c131a1f98df0e04412617d7ad72
Petromir Kolev <petromirkolev@gmail.com>
2026-01-23 19:22:21 +0200

    chore: update config file

--- playwright.config.js
+++ playwright.config.js
@@ -2,14 +2,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
@@ -24,58 +16,28 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL:
-      process.env.BASE_URL ||
-      'https://petromirkolev.github.io/js-foundations-projects/projects/odin-weather/index.html',
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    ...devices['Desktop Chrome'],
+    baseURL: 'https://example.com', // default fallback, overridden per project
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'todo',
+      testDir: 'tests/odin-todo',
+      use: {
+        baseURL:
+          'https://petromirkolev.github.io/js-foundations-projects/projects/odin-todo/',
+      },
     },
-
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'weather',
+      testDir: 'tests/odin-weather',
+      use: {
+        baseURL:
+          'https://petromirkolev.github.io/js-foundations-projects/projects/odin-weather/',
+      },
     },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run serve:todo',
-  //   url: 'http://127.0.0.1:5500',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
